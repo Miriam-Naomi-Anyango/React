498b1a18aa5b0c518b4152c2cd9746dbc509a4e8
Chris <minaoanya@gmail.com>
2023-02-06 14:23:52 +0300

    Doing it

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home.css from
+
+export function Home(props) {
+    
+
+    return (
+        <>
+           <div className='nail'>Am doing it </div> 
+        </>
+    )
+}
